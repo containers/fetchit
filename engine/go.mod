7d78059b7038c37dcf5dfb7d63f16a61af0b8692
Ryan Cook <rcook@redhat.com>
2022-02-14 12:48:37 -0500

    Listing file and apply diff (#5)
    * listing files
    
    * cron integration and file lists
    
    * need change
    
    * patch log message
    
    * port change
    
    * port change to test
    
    * port change
    
    * reset port
    
    * test changeg
    
    * set improper pull
    
    * working port
    
    * working port
    
    * reset port
    
    * errors logs and continuous running

--- engine/go.mod
+++ engine/go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/containers/common v0.47.2 // indirect
 	github.com/containers/libpod v1.9.3
 	github.com/containers/podman/v4 v4.0.0-rc4
+	github.com/go-co-op/gocron v1.12.0 // indirect
 	github.com/go-git/go-git/v5 v5.4.2
 )
--- engine/main.go
+++ engine/main.go
@@ -6,8 +6,14 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/go-co-op/gocron"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 type Repo struct {
@@ -16,6 +22,7 @@ type Repo struct {
 	Branch       string
 	Method       string
 	Subdirectory string
+	Schedule     string
 }
 
 func main() {
@@ -26,13 +33,54 @@ func main() {
 	var repo Repo
 	json.Unmarshal([]byte(repoJson), &repo)
 
+	s := gocron.NewScheduler(time.UTC)
+	s.Cron(repo.Schedule).Do(process)
+	s.StartAsync()
+	select {}
+}
+
+func process() {
+	repoJson, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var repo Repo
+	json.Unmarshal([]byte(repoJson), &repo)
+
 	if _, err := os.Stat(repo.Directory); os.IsNotExist(err) {
 		fmt.Printf("git clone %s %s --recursive\n", repo.Url, repo.Branch)
 
-		_, err := git.PlainClone(repo.Directory, false, &git.CloneOptions{
-			URL:        repo.Url,
-			RemoteName: repo.Branch,
+		r, err := git.PlainClone(repo.Directory, false, &git.CloneOptions{
+			URL:           repo.Url,
+			ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", repo.Branch)),
+			SingleBranch:  true,
 		})
+		if err != nil {
+			fmt.Printf("Error while cloning the repository: %s\n", err)
+		}
+		ref, err := r.Head()
+		if err != nil {
+			fmt.Printf("Error when retrieving head: %s\n", err)
+		}
+
+		// ... retrieving the commit object
+		commit, err := r.CommitObject(ref.Hash())
+		if err != nil {
+			fmt.Printf("Error when retrieving commit: %s\n", err)
+		}
+
+		// ... retrieve the tree from the commit
+		tree, err := commit.Tree()
+
+		// ... get the files iterator and print the file
+		tree.Files().ForEach(func(f *object.File) error {
+			if strings.Contains(f.Name, repo.Subdirectory) {
+				path := repo.Directory + "/" + f.Name
+				rawPodman(path)
+			}
+			return nil
+		})
+
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -51,18 +99,59 @@ func main() {
 			log.Fatal(err)
 		}
 
-		// Pull the latest changes from the origin remote and merge into the current branch
-		err = w.Pull(&git.PullOptions{RemoteName: repo.Branch})
+		ref, err := r.Head()
 		if err != nil {
-			log.Fatal(err)
+			fmt.Printf("Error when retrieving head: %s\n", err)
 		}
 
+		// ... retrieving the commit object
+		prevCommit, err := r.CommitObject(ref.Hash())
+		if err != nil {
+			fmt.Printf("Error when retrieving commit: %s\n", err)
+		}
+
+		// ... retrieve the tree from the commit
+		prevTree, err := prevCommit.Tree()
+		if err != nil {
+			fmt.Printf("Error while generating tree: %s\n", err)
+		}
+
+		// Pull the latest changes from the origin remote and merge into the current branch
+		err = w.Pull(&git.PullOptions{
+			ReferenceName: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", repo.Branch)),
+			SingleBranch:  true,
+		})
+		if err != nil {
+			fmt.Printf("Nothing to pull.....Requeuing \n")
+		} else {
+			// Print the latest commit that was just pulled
+			ref, err := r.Head()
+			if err != nil {
+				fmt.Printf("An error has occured %s\n", err)
+			}
+			commit, err := r.CommitObject(ref.Hash())
+			if err != nil {
+				fmt.Printf("Error when retrieving commit: %s\n", err)
+			}
+
+			// ... retrieve the tree from the commit
+			tree, err := commit.Tree()
+			if err != nil {
+				fmt.Printf("Error while generating tree: %s\n", err)
+			}
+
+			changes, err := tree.Diff(prevTree)
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, change := range changes {
+				if strings.Contains(change.To.Name, repo.Subdirectory) {
+					path := repo.Directory + "/" + change.To.Name
+					rawPodman(path)
+				}
+			}
+		}
 	} else {
-		fmt.Printf("%s exists but is not a git repository", repo.Directory)
-	}
-	fmt.Printf("Deploying items in %s format\n", repo.Method)
-	if repo.Method == "raw" {
-		path := repo.Directory + "/" + repo.Subdirectory
-		rawPodman(path)
+		fmt.Printf("%s exists but is not a git repository", repo.Url)
 	}
 }
