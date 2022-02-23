7d77c22165e852a7e4de39c3b235b208bf79e985
Ryan Cook <rcook@redhat.com>
2022-02-23 14:31:32 -0500

    commit base systemd starting

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.15
 
 require (
 	github.com/containers/common v0.47.2
-	github.com/containers/podman/v4 v4.0.0-rc4
+	github.com/containers/podman/v4 v4.0.0
 	github.com/go-co-op/gocron v1.12.0
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/opencontainers/runtime-spec v1.0.3-0.20211214071223-8958f93039ab
--- pkg/engine/systemd.go
+++ pkg/engine/systemd.go
@@ -3,7 +3,6 @@ package engine
 import (
 	"context"
 	"fmt"
-	"io"
 	"os"
 	"strings"
 
@@ -15,29 +14,11 @@ import (
 
 func systemdPodman(path string) error {
 	fmt.Printf("Deploying systemd file(s) %s\n", path)
-	in, err := os.Open(path)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error opening file: %s\n", err)
-	}
-	defer in.Close()
 	systemdFile := path[strings.LastIndex(path, "/")+1:]
-	out, err := os.Create("/etc/systemd/system/" + systemdFile)
-	if err != nil {
-		fmt.Printf("Error creating file: %s\n", err)
-	}
-	defer func() {
-		cerr := out.Close()
-		if err == nil {
-			err = cerr
-		}
-	}()
-	if _, err = io.Copy(out, in); err != nil {
-		fmt.Printf("Error copying file: %s\n", err)
-	}
-	err = out.Sync()
-	if err != nil {
-		fmt.Printf("Error syncing file: %s\n", err)
-	}
+
+	systemdLocation := "/etc/systemd/system/"
+	copyFile := ("/opt/" + path + " " + "/host" + systemdLocation)
+
 	// Create a new Podman client
 	conn, err := bindings.NewConnection(context.Background(), "unix://run/podman/podman.sock")
 	if err != nil {
@@ -45,20 +26,22 @@ func systemdPodman(path string) error {
 		os.Exit(1)
 	}
 
-	s := specgen.NewSpecGenerator("quay.io/harpoon/harpoon-amd:latest", false)
-	s.Name = "systemd"
+	s := specgen.NewSpecGenerator("quay.io/harpoon/harpoon:latest", false)
+	s.Name = "systemd" + systemdFile
 	s.Privileged = true
 	s.PidNS = specgen.Namespace{
 		NSMode: "host",
 		Value:  "",
 	}
-	s.Command = []string{"chroot", "/host", "sh", "-c", "systemctl restart " + systemdFile}
-	s.Mounts = []specs.Mount{{Source: "/run", Destination: "/run", Type: "bind", Options: []string{"rw"}}, {Source: "/", Destination: "/host", Type: "bind", Options: []string{"ro"}}}
+	s.Command = []string{"sh", "-c", "cp " + copyFile, "chroot", "/host", "sh", "-c", "systemctl restart " + systemdFile}
+	s.Mounts = []specs.Mount{{Source: "/run", Destination: "/run", Type: "bind", Options: []string{"rw"}}, {Source: "/", Destination: "/host", Type: "bind", Options: []string{"rw"}}}
+	s.Volumes = []*specgen.NamedVolume{{Name: "harpoon-volume", Dest: "/opt", Options: []string{"ro"}}}
 	createResponse, err := containers.CreateWithSpec(conn, s, nil)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	fmt.Printf("command to run %s\n", s.Command)
 	fmt.Println("Container created.")
 	if err := containers.Start(conn, createResponse.ID, nil); err != nil {
 		fmt.Println(err)
