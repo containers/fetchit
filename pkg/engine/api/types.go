d57a2168a2dad29c46b6e38129b990c331293e2a
Sally O'Malley <somalley@redhat.com>
2022-03-09 16:07:32 -0500

    fix the nested target map, finish renaming repo -> target, fix findDiff

--- pkg/engine/api/types.go
+++ pkg/engine/api/types.go
@@ -14,31 +14,33 @@ type Target struct {
 	Systemd      Systemd      `mapstructure:"systemd"`
 	Kube         Kube         `mapstructure:"kube"`
 	FileTransfer FileTransfer `mapstructure:"fileTransfer"`
-	Mu           sync.Mutex
+	// map of method:schedule
+	MethodSchedules map[string]string
+	Mu              sync.Mutex
 }
 
 type Raw struct {
 	TargetPath string `mapstructure:"targetPath"`
-	Schedule     string `mapstructure:"schedule"`
-	InitialRun   bool
+	Schedule   string `mapstructure:"schedule"`
+	InitialRun bool
 }
 
 type Systemd struct {
 	TargetPath string `mapstructure:"targetPath"`
-	User         string `mapstructure:"user"`
-	Schedule     string `mapstructure:"schedule"`
-	InitialRun   bool
+	User       string `mapstructure:"user"`
+	Schedule   string `mapstructure:"schedule"`
+	InitialRun bool
 }
 
 type FileTransfer struct {
 	TargetPath string `mapstructure:"targetPath"`
-	Dest         string `mapstructure:"dest"`
-	Schedule     string `mapstructure:"schedule"`
-	InitialRun   bool
+	Dest       string `mapstructure:"dest"`
+	Schedule   string `mapstructure:"schedule"`
+	InitialRun bool
 }
 
 type Kube struct {
 	TargetPath string `mapstructure:"targetPath"`
-	Schedule     string `mapstructure:"schedule"`
-	InitialRun   bool
+	Schedule   string `mapstructure:"schedule"`
+	InitialRun bool
 }
