90afc8c01704eeb543992103b07c7157fd0162f3
Joseph Sawaya <jsawaya@redhat.com>
2022-03-17 15:30:43 -0400

    file transfer method removal capability (#102)
    This commit adds the file removal feature
    to the file transfer method.
    
    Signed-off-by: Joseph Sawaya <jsawaya@redhat.com>

--- pkg/engine/filetransfer.go
+++ pkg/engine/filetransfer.go
@@ -3,13 +3,13 @@ package engine
 import (
 	"context"
 	"fmt"
-	"os"
 	"path/filepath"
 
 	"github.com/containers/podman/v4/libpod/define"
 	"github.com/containers/podman/v4/pkg/bindings"
 	"github.com/containers/podman/v4/pkg/bindings/containers"
 	"github.com/containers/podman/v4/pkg/bindings/images"
+	"github.com/containers/podman/v4/pkg/domain/entities"
 	"github.com/containers/podman/v4/pkg/specgen"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/redhat-et/harpoon/pkg/engine/api"
@@ -17,12 +17,9 @@ import (
 	"k8s.io/klog/v2"
 )
 
-func fileTransferPodman(ctx context.Context, path, dest, method string, target *api.Target) error {
-	klog.Infof("Deploying file(s) %s", path)
-	file := filepath.Base(path)
+const stopped = define.ContainerStateStopped
 
-	source := filepath.Join("/opt", path)
-	copyFile := (source + " " + dest)
+func fileTransferPodman(ctx context.Context, path, dest, method string, target *api.Target, prev *string) error {
 
 	// Create a new Podman client
 	conn, err := bindings.NewConnection(ctx, "unix://run/podman/podman.sock")
@@ -30,10 +27,85 @@ func fileTransferPodman(ctx context.Context, path, dest, method string, target *
 		return fmt.Errorf("error podman socket: %w", err)
 	}
 
+	if prev != nil {
+		pathToRemove := filepath.Join(dest, filepath.Base(*prev))
+		s := generateSpecRemove(method, filepath.Base(pathToRemove), pathToRemove, dest, target)
+		createResponse, err := createAndStartContainer(conn, s)
+		if err != nil {
+			return err
+		}
+
+		// TODO: check file got removed
+
+		err = waitAndRemoveContainer(conn, createResponse.ID)
+		if err != nil {
+			return err
+		}
+	}
+
+	if path == "" {
+		return nil
+	}
+
+	klog.Infof("Deploying file(s) %s", path)
+
+	file := filepath.Base(path)
+
+	source := filepath.Join("/opt", path)
+	copyFile := (source + " " + dest)
+
 	klog.Infof("Identifying if image exists locally")
 	// Pull image if it doesn't exist
-	var present bool
-	present, err = images.Exists(conn, harpoonImage, nil)
+	err = fetchImage(conn)
+	if err != nil {
+		return err
+	}
+
+	s := generateSpec(method, file, copyFile, dest, target)
+	createResponse, err := createAndStartContainer(conn, s)
+	if err != nil {
+		klog.Infof("createError: %s\n", err)
+		return err
+	}
+
+	//TODO: check file got put in the right place
+
+	// Wait for the container to exit
+	err = waitAndRemoveContainer(conn, createResponse.ID)
+
+	return err
+}
+
+func generateSpec(method, file, copyFile, dest string, target *api.Target) *specgen.SpecGenerator {
+	s := specgen.NewSpecGenerator(harpoonImage, false)
+	s.Name = method + "-" + target.Name + "-" + file
+	s.Privileged = true
+	s.PidNS = specgen.Namespace{
+		NSMode: "host",
+		Value:  "",
+	}
+	s.Command = []string{"sh", "-c", "cp " + copyFile}
+	s.Mounts = []specs.Mount{{Source: dest, Destination: dest, Type: "bind", Options: []string{"rw"}}}
+	s.Volumes = []*specgen.NamedVolume{{Name: harpoonVolume, Dest: "/opt", Options: []string{"ro"}}}
+	return s
+}
+
+func generateSpecRemove(method, file, pathToRemove, dest string, target *api.Target) *specgen.SpecGenerator {
+	s := specgen.NewSpecGenerator(harpoonImage, false)
+	s.Name = method + "-" + target.Name + "-" + file
+	s.Privileged = true
+	s.PidNS = specgen.Namespace{
+		NSMode: "host",
+		Value:  "",
+	}
+	s.Command = []string{"sh", "-c", "rm " + pathToRemove}
+	s.Mounts = []specs.Mount{{Source: dest, Destination: dest, Type: "bind", Options: []string{"rw"}}}
+	s.Volumes = []*specgen.NamedVolume{{Name: harpoonVolume, Dest: "/opt", Options: []string{"ro"}}}
+	return s
+}
+
+func fetchImage(conn context.Context) error {
+	present, err := images.Exists(conn, harpoonImage, nil)
 	klog.Infof("Is image present? %t", present)
 	if err != nil {
 		return err
@@ -46,36 +118,37 @@ func fileTransferPodman(ctx context.Context, path, dest, method string, target *
 		}
 	}
 
-	s := specgen.NewSpecGenerator(harpoonImage, false)
-	s.Name = method + "-" + target.Name + "-" + file
-	s.Privileged = true
-	s.PidNS = specgen.Namespace{
-		NSMode: "host",
-		Value:  "",
-	}
-	s.Command = []string{"sh", "-c", "cp " + copyFile}
-	s.Mounts = []specs.Mount{{Source: dest, Destination: dest, Type: "bind", Options: []string{"rw"}}}
-	s.Volumes = []*specgen.NamedVolume{{Name: harpoonVolume, Dest: "/opt", Options: []string{"ro"}}}
+	return nil
+}
+
+func createAndStartContainer(conn context.Context, s *specgen.SpecGenerator) (entities.ContainerCreateResponse, error) {
 	createResponse, err := containers.CreateWithSpec(conn, s, nil)
+	if err != nil {
+		return createResponse, err
+	}
+
+	if err := containers.Start(conn, createResponse.ID, nil); err != nil {
+		return createResponse, err
+	}
+	klog.Infof("Container %s created.", s.Name)
+
+	return createResponse, nil
+}
+
+func waitAndRemoveContainer(conn context.Context, ID string) error {
+	_, err := containers.Wait(conn, ID, new(containers.WaitOptions).WithCondition([]define.ContainerStatus{stopped}))
 	if err != nil {
 		return err
 	}
-	if err := containers.Start(conn, createResponse.ID, nil); err != nil {
+
+	_, err = containers.Remove(conn, ID, new(containers.RemoveOptions).WithForce(true))
+	if err != nil {
+		// There's a podman bug somewhere that's causing this
+		if err.Error() == "unexpected end of JSON input" {
+			return nil
+		}
 		return err
 	}
-	klog.Infof("Container %s created.", s.Name)
-	var (
-		stopped = define.ContainerStateStopped
-	)
 
-	destFile := filepath.Join(dest, file)
-	if _, err := os.Stat(destFile); err == nil {
-		klog.Infof("Transferred file(s) %s successfully.", file)
-	} else {
-		err = fmt.Errorf("Transferred file %s not in expected location after processing target: %s: %v", destFile, target.Name, err)
-	}
-	// Wait for the container to exit
-	containers.Wait(conn, createResponse.ID, new(containers.WaitOptions).WithCondition([]define.ContainerStatus{stopped}))
-	containers.Remove(conn, createResponse.ID, new(containers.RemoveOptions).WithForce(true))
-	return err
+	return nil
 }
