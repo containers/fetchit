e9bd52e3c7e6ad3a75e2709a04352710a75be94d
Joseph Sawaya <jsawaya@redhat.com>
2022-03-25 15:26:42 -0400

    Refactor kube method
    This commit refactors the kube method to separate
    the functionality better between the helper functions.
    
    Signed-off-by: Joseph Sawaya <jsawaya@redhat.com>

--- pkg/engine/kube.go
+++ pkg/engine/kube.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/containers/podman/v4/pkg/bindings/play"
 	"github.com/containers/podman/v4/pkg/bindings/pods"
+	"github.com/containers/podman/v4/pkg/domain/entities"
 	"github.com/redhat-et/harpoon/pkg/engine/utils"
 
 	"gopkg.in/yaml.v3"
@@ -25,9 +26,9 @@ func kubePodman(ctx context.Context, mo *FileMountOptions) error {
 	}
 
 	if mo.Previous != nil {
-		err := stopPods(mo.Conn, []byte(*mo.Previous))
+		err := stopPods(ctx, []byte(*mo.Previous))
 		if err != nil {
-			return utils.WrapErr(err, "Error stopping pod")
+			return utils.WrapErr(err, "Error stopping pods")
 		}
 	}
 
@@ -36,9 +37,10 @@ func kubePodman(ctx context.Context, mo *FileMountOptions) error {
 		if err != nil {
 			return utils.WrapErr(err, "Error reading file")
 		}
-		err = stopPods(mo.Conn, kubeYaml)
+
+		err = stopPods(ctx, kubeYaml)
 		if err != nil {
-			return utils.WrapErr(err, "Error stopping pod")
+			return utils.WrapErr(err, "Error stopping pods")
 		}
 
 		err = createPods(mo.Conn, mo.Path, kubeYaml)
@@ -50,122 +52,151 @@ func kubePodman(ctx context.Context, mo *FileMountOptions) error {
 	return nil
 }
 
-// This should be deprecated in favour of play.KubeDown() at some point
-func stopPods(ctx context.Context, specs []byte) error {
-	pod_list, err := podsToStop([]byte(specs))
+func stopPods(conn context.Context, input []byte) error {
+	podList, err := podFromBytes(input)
 	if err != nil {
-		return utils.WrapErr(err, "Error getting list of pods to stop")
+		return utils.WrapErr(err, "Error getting list of pods in spec")
 	}
 
-	var pod_map = make(map[string]bool)
-
-	for _, pod := range pod_list {
-		pod_map[pod] = true
+	podNameList, err := getPodNames(podList)
+	if err != nil {
+		return utils.WrapErr(err, "Error getting list of pod names")
 	}
 
-	report, err := pods.List(ctx, nil)
+	podMap := podMapFromList(podNameList)
+
+	report, err := pods.List(conn, nil)
 	if err != nil {
 		return utils.WrapErr(err, "Error getting list of pods from podman")
 	}
-	for _, p := range report {
-		if _, ok := pod_map[p.Name]; ok {
-			klog.Infof("Tearing down pod: %s\n", p.Name)
-			err = tearDownPods(ctx, p.Name)
-			if err != nil {
-				return utils.WrapErr(err, "error tearing down pod %s", p.Name)
-			}
-		}
+
+	runningPodNameList := reportToPodNameList(report)
+
+	podsToBeDeleted := filterPods(runningPodNameList, podMap)
+
+	err = tearDownPods(conn, podsToBeDeleted)
+	if err != nil {
+		return utils.WrapErr(err, "Error tearing down pods")
 	}
 
 	return nil
 }
 
-func tearDownPods(ctx context.Context, podName string) error {
-	_, err := pods.Stop(ctx, podName, nil)
-	if err != nil {
-		return utils.WrapErr(err, "error stopping pod %s", podName)
+func podMapFromList(podNameList []string) map[string]bool {
+	podMap := make(map[string]bool)
+	for _, pod := range podNameList {
+		podMap[pod] = true
 	}
-	_, err = pods.Remove(ctx, podName, nil)
-	if err != nil {
-		return utils.WrapErr(err, "error removing pod %s", podName)
+	return podMap
+}
+
+func reportToPodNameList(report []*entities.ListPodsReport) []string {
+	ret := make([]string, 0)
+	for _, r := range report {
+		ret = append(ret, r.Name)
+	}
+	return ret
+}
+
+func filterPods(runningPodNameList []string, podMap map[string]bool) []string {
+	ret := make([]string, 0)
+	for _, p := range runningPodNameList {
+		if _, ok := podMap[p]; ok {
+			ret = append(ret, p)
+		}
 	}
 
+	return ret
+}
+
+func tearDownPods(ctx context.Context, podNameList []string) error {
+	for _, podName := range podNameList {
+		_, err := pods.Stop(ctx, podName, nil)
+		if err != nil {
+			return utils.WrapErr(err, "Error stopping pod %s", podName)
+		}
+		_, err = pods.Remove(ctx, podName, nil)
+		if err != nil {
+			return utils.WrapErr(err, "Error removing pod %s", podName)
+		}
+	}
 	return nil
 }
 
 func createPods(ctx context.Context, path string, specs []byte) error {
 	pod_list, err := podFromBytes(specs)
 	if err != nil {
-		return utils.WrapErr(err, "error getting list of pods in spec")
+		return utils.WrapErr(err, "Error getting list of pods in spec")
 	}
 
 	for _, pod := range pod_list {
 		err = validatePod(pod)
 		if err != nil {
-			return utils.WrapErr(err, "error validating pod spec")
+			return utils.WrapErr(err, "Error validating pod spec")
 		}
 	}
 
 	_, err = play.Kube(ctx, path, nil)
 	if err != nil {
-		return utils.WrapErr(err, "error playing kube spec")
+		return utils.WrapErr(err, "Error playing kube spec")
 	}
 
 	klog.Infof("Created pods from spec in %s\n", path)
 	return nil
 }
 
-func podsToStop(curr []byte) ([]string, error) {
-	pod_list, err := podFromBytes(curr)
-	if err != nil {
-		return nil, utils.WrapErr(err, "error getting list of pods in spec")
-	}
-
+func getPodNames(podList []v1.Pod) ([]string, error) {
 	ret := make([]string, 0)
-	for _, pod := range pod_list {
-		ret = append(ret, pod.ObjectMeta.Name)
+
+	for _, pod := range podList {
+		if pod.ObjectMeta.Name != "" {
+			ret = append(ret, pod.ObjectMeta.Name)
+		} else {
+			return nil, errors.New("pod has no name")
+		}
 	}
 
 	return ret, nil
 }
 
 func podFromBytes(input []byte) ([]v1.Pod, error) {
-	var i interface{}
 	var t metav1.TypeMeta
-	pod := v1.Pod{}
 	d := yaml.NewDecoder(bytes.NewReader(input))
 	ret := make([]v1.Pod, 0)
+
 	for {
+		var i interface{}
 		err := d.Decode(&i)
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			return ret, utils.WrapErr(err, "error decoding yaml")
+			return ret, utils.WrapErr(err, "Error decoding yaml")
 		}
 
 		o, err := yaml.Marshal(i)
 		if err != nil {
-			return ret, utils.WrapErr(err, "error marshalling yaml into object for conversion to json")
+			return ret, utils.WrapErr(err, "Error marshalling yaml into object for conversion to json")
 		}
 
 		b, err := k8syaml.YAMLToJSON(o)
 		if err != nil {
-			return ret, utils.WrapErr(err, "error converting yaml to json")
+			return ret, utils.WrapErr(err, "Error converting yaml to json")
 		}
 
 		err = json.Unmarshal(b, &t)
 		if err != nil {
-			return ret, utils.WrapErr(err, "error unmarshalling json object")
+			return ret, utils.WrapErr(err, "Error unmarshalling json object")
 		}
 
 		if t.Kind != "Pod" {
 			continue
 		}
 
+		pod := v1.Pod{}
 		err = json.Unmarshal(b, &pod)
 		if err != nil {
-			return ret, utils.WrapErr(err, "error unmarshalling json into pod object")
+			return ret, utils.WrapErr(err, "Error unmarshalling json into pod object")
 		}
 
 		ret = append(ret, pod)
